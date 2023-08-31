e0d66893cb6b2dc0a48266f8aca75c3d3601e868
Afonso Filho <afonso.rf@gmail.com>
2023-08-31 10:03:41 -0300

    :recycle: refactor: ações repetitivas passadas para funcoes no get_items

--- zabbix_items/get_items.py
+++ zabbix_items/get_items.py
@@ -36,90 +36,80 @@ def csv_to_list(filecsv, delimiter=","):
 
     return result
 
-try:
-    url_list = csv_to_list(file_url)
+def get_url():
     try:
-        if len(url_list[1:]) > 1:
-            while True:
-                    os.system("clear")
-                    print("#" * 40)
-                    print(f"#{'Escolha um dos servidores abaixo':^38}#")
-                    print("#" * 40)
-                    print()
-                    for i in range(1,len(url_list)):
-                        print(f" [ {i} ] - Zabbix {url_list[i][0].upper()}")
-                    print()
-                    index = input("Escolha o zabbix desejado (Crtl+C sair): ").strip()
-                    if index.isdigit() and 0 < int(index) < len(url_list):
-                        server = int(index)
-                        break
-                    else:
-                        print(f"Invalid `{index}`")
-                        print(f"Escolha entre 1 a {len(url_list) - 1}.")
-                        sleep(1.5)
-            
-        else:
-            server = 1
-    except Exception as error:
-        print(error)
-    
-    try:
-        zabbixname = url_list[server][0].strip().upper()
-        url = ''
-        for item in url_list[server]:
-            if "://" in item:
-                url = item
-        if not url:
-            url = input("Url Zabbix: ")
-    except IndexError:
-        url = input("Url Zabbix: ")
-    
-    try:
-        api_token = ""
-        if url_list[server][2]:
-            api_token =  url_list[server][2].strip()
-            username = ""
-            passwd = ""
-        if not api_token:
-            while True:
-                username = input("Zabbix User: ")  
-                passwd = getpass("Password")
-                if username and passwd:
-                    break
-    except IndexError:
-        while True:
-            username = input("Zabbix User: ")  
-            passwd = getpass("Password: ")
-            api_token = ""
-            if username and passwd:
-                break
-except FileNotFoundError:
-    try:
-        while True:
-            url = input("Url Zabbix: ")
+        while True: # TODO: Adicionar banner de apresentação
+            url = input("Url Zabbix (Crtl+C to exit): ")
             if "://" in url:
-                break
+                return url
             else:
                 print(f"Url invalid {url}")
     except KeyboardInterrupt:
-        print("\nGood bye.")
-        sys.exit(1)
+        print("\nGood bye!")
+        sys.exit()
+ 
+def get_user_passwd():
     try:
-        while True:
-            username = input("Zabbix User: ")  
-            passwd = getpass("Password: ")
+        while True: # TODO: Adicionar banner de apresentação
+            username = input("Zabbix User: ").strip() 
+            passwd = getpass("Password: ").strip()
             if username and passwd:
-                break
+                return username, passwd
     except KeyboardInterrupt:
-        print("\nGood bye.")
-        sys.exit(1)
+        print("\nGood bye!")
+        sys.exit()
 
-except KeyboardInterrupt:
-    print("Good bye.")
-    sys.exit(1)
+try:
+    url_list = csv_to_list(file_url)
+except FileNotFoundError:
+    url_list = [("zabbix_name", "url")]
+    url = get_url()
+    url_list.append(("unknown", url))
 except Exception as error:
         print(error)
         sys.exit(1)
+
+if len(url_list[1:]) > 1:
+    while True:
+            os.system("clear")
+            print("#" * 40)
+            print(f"#{'Choose one of the servers below':^38}#")
+            print("#" * 40)
+            print()
+            for i in range(1,len(url_list)):
+                print(f" [ {i} ] - Zabbix {url_list[i][0].upper()}")
+            print()
+            index = input("Choose the desired zabbix (Crtl+C to exit): ").strip()
+            if index.isdigit() and 0 < int(index) < len(url_list):
+                server = int(index)
+                break
+            else:
+                print(f"Invalid `{index}`")
+                print(f"Escolha entre 1 a {len(url_list) - 1}.")
+                sleep(1.5)
+else:
+    server = 1
+
+try:
+    zabbixname = url_list[server][0].strip().upper()
+except IndexError:
+    zabbixname = "UNKONWN"
+try:
+    url = url_list[server][1].strip()
+    if "://" not in url:
+        url = get_url()
+except IndexError:
+    url = get_url()
+
+try:
+    api_token = ""
+    if url_list[server][2].strip():
+        api_token =  url_list[server][2].strip()
+except IndexError:
+    api_token = ""
+
+if not api_token:
+    username, passwd = get_user_passwd()
 
 # Zabbix connect
 while True:
@@ -133,22 +123,22 @@ while True:
         print(f"Connected to Zabbix {zabbixname} API Version {zapi.api_version()}.")
         sleep(1)
         break
-    except KeyboardInterrupt:
-        print("\nGood bye.")
-        sys.exit(1)
     except Exception as error:
-        if "Login name or password is incorrect" in str(error):
+        if "name or password" in str(error): 
             print("Login name or password is incorrect")
-            username = input("Zabbix User: ")  
-            passwd = getpass("Password: ")
-        elif "404 Client Error: Not Found for url" in str(error):
-            print("404 Client Error: Not Found for url")
-            url = input("Url Zabbix: ")
+            username, passwd = get_user_passwd()
+        elif "Not Found for url" in str(error):
+            print("Not Found for url")
+            url = get_url()
         elif "Invalid URL" in str(error):
-            print(f"Invalid URL `{url}`.")
-            url = input("Url Zabbix: ")
+            print(str(error))
+            url = get_url()
+        elif "Failed to establish a new connection" in str(error):
+            print(f"Failed to establish a new connection `{url}`.")
+            url = get_url()
         else:
             print(str(error)) 
+            sys.exit(1)
 
 
 # Collecting the hosts
@@ -163,63 +153,69 @@ get_hosts = zapi.host.get(
 
 hosts = {info["hostid"]: info["host"] for info in tqdm(get_hosts, ncols=70)}
 
-print(f"Collecting item info from {len(hosts)} hosts")
-get_items = []
-for info in tqdm(get_hosts, ncols=70):
-    item = zapi.item.get(
-        hostids=info["hostid"],
-        output=["hostid", "key_", "name", "lastvalue"],
-        tags=item_tags,
-    )
-    if item:
-        get_items.append(item)
+print(f"Collecting item info from {len(hosts)} hosts. (Crtl+C to exit)")
+try:
+    get_items = []
+    for info in tqdm(get_hosts, ncols=70):
+        item = zapi.item.get(
+            hostids=info["hostid"],
+            output=["hostid", "key_", "name", "lastvalue"],
+            tags=item_tags,
+        )
+        if item:
+            get_items.append(item)
 
-print(f"Organizing the collected information ({len(get_items)}).")
-onu_list = []
-for info in tqdm(get_items, ncols=70):
-    if info:
-        onus = []
-        onu_ids = set()
-        for item in info:
-            if item:
-                onu_id = re.search("\[(.*)\]", item["key_"]).group(1)
-                if onu_id in onu_ids:
-                    for onu in onus:
-                        if onu_id == onu["id"]:
-                            if not "sinal" in onu:
-                                onu["sinal"] = item["lastvalue"]
-                            if not "pon" in onu:
-                                onu["pon"] = item["lastvalue"]
-                            break
-                else:
-                    onu = dict()
-                    onu["id"] = onu_id
-                    if "sinalonu" in item["key_"].lower():
-                        onu["sinal"] = item["lastvalue"]
-                    if "ponport" in item["key_"].lower():
-                        onu["pon"] = item["lastvalue"]
-                    onu["name"] = re.search(":: (.*) ::", item["name"]).group(1).strip()
-                    onu["host"] = hosts[item["hostid"]]
-                    onus.append(onu)
-                    onu_ids.add(onu_id)
-
-        if onus:
-            onu_list.append(onus)
-
-print("Preparing the info for writing")
-list_result = []  # HOST, ONU, SINAL, PON
-for host in tqdm(range(len(onu_list)), ncols=70):
-    for info in onu_list[host]:
+    print(f"Organizing the collected information ({len(get_items)}).")
+    onu_list = []
+    for info in tqdm(get_items, ncols=70):
         if info:
-            list_result.append(
-                [
-                    info["host"],
-                    info["name"],
-                    round(float(info["sinal"]), 2) if "sinal" in info else "",
-                    info["pon"] if "pon" in info else "",
-                ]
-            )
+            onus = []
+            onu_ids = set()
+            for item in info:
+                if item:
+                    onu_id = re.search("\[(.*)\]", item["key_"]).group(1)
+                    if onu_id in onu_ids:
+                        for onu in onus:
+                            if onu_id == onu["id"]:
+                                if not "sinal" in onu:
+                                    onu["sinal"] = item["lastvalue"]
+                                if not "pon" in onu:
+                                    onu["pon"] = item["lastvalue"]
+                                break
+                    else:
+                        onu = dict()
+                        onu["id"] = onu_id
+                        if "sinalonu" in item["key_"].lower():
+                            onu["sinal"] = item["lastvalue"]
+                        if "ponport" in item["key_"].lower():
+                            onu["pon"] = item["lastvalue"]
+                        onu["name"] = re.search(":: (.*) ::", item["name"]).group(1).strip()
+                        onu["host"] = hosts[item["hostid"]]
+                        onus.append(onu)
+                        onu_ids.add(onu_id)
 
+            if onus:
+                onu_list.append(onus)
+
+    print("Preparing the info for writing")
+    list_result = []  # HOST, ONU, SINAL, PON
+    for host in tqdm(range(len(onu_list)), ncols=70):
+        for info in onu_list[host]:
+            if info:
+                list_result.append(
+                    [
+                        info["host"],
+                        info["name"],
+                        round(float(info["sinal"]), 2) if "sinal" in info else "",
+                        info["pon"] if "pon" in info else "",
+                    ]
+                )
+except KeyboardInterrupt:
+    print("\nGood bye!")
+    sys.exit()
+except Exception as error:
+    print(str(error))
+    sys.exit(1)    
 
 today = datetime.strftime(datetime.now(), "%Y-%m-%d")
 file_csv = f"onu_sinal_{today}"
@@ -252,5 +248,4 @@ else:
             if len(list_result[row]):
                 file_.write(",".join(list_result[row]) + "\n")
                 
-
 print('Completed successfully.')
