0269c582f78f56bf81789c0de297c2a573c3e7fb
Afonso Filho <afonso.rf@gmail.com>
2024-02-09 14:22:17 -0300

    :recycle: refactor: nomes de variaveis

--- modules/csv.py
+++ modules/csv.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 
-def csv_to_list(file_csv, delimiter=","):
+def csv_to_list(file_csv, delimiter=",") -> list:
     result = []
     for line in open(file_csv, encoding="UTF-8"):
         line = line.strip()
         if "#" not in line[:4]:
-            user = line.split(delimiter)
-            if len(user) > 1:
-                result.append(user)
+            info = line.split(delimiter)
+            if len(info) > 1:
+                result.append(info)
 
     return result
